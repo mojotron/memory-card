39084fe57cdf54a1f523dea491e50fc23269ce2b
mojotron <igormalj@gmail.com>
2022-05-20 18:16:17 +0200

    Add score and general style

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header(props) {
+  return (
+    <header className="Header">
+      <h1 className="Header__headings">Memory Cards</h1>
+      <div className="Header__scoreboard">
+        <h2>Current Score: {props.currentScore}</h2>
+        <h2>High Score: {props.highScore}</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
