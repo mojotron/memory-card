5994f1a46cafe767a1499b1af0309cf94dd4c25f
mojo <igormalj@gmail.com>
2024-02-05 17:53:10 +0100

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/memory-card/',
   plugins: [react()],
   test: {
     globals: true,
